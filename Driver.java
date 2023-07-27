2930138ecb6b62edf3b33b1e9db320ebcc1cfeca
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2023-07-27 09:52:00 +0800

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,15 @@
+/** This is the main driver class
+ * @author Joshua Tipon
+ * @version Prototype MC02
+ * @since June 29
+*/
+
+import java.util.Scanner;
+
+public class Driver {
+      static Scanner sc = new Scanner(System.in);
+      
+      public static void main(String[] args) {
+
+      }
+}
